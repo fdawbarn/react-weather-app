7cc40a598d95a3f7600a3fbd8b6ff576a404ac5c
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-08-29 17:28:52 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" >
+      <div className="container">
       <h1>
         Weather app
       </h1>
@@ -12,6 +13,7 @@ function App() {
         This project was coded by Freya Dawbarn and is {""}
         <a href="https://github.com/fdawbarn/react-weather-app" target="_blank"  rel="noreferrer">open-sourced on Github</a>
       </footer>
+      </div>
     </div>
   );
 }
