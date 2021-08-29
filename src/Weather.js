53bfdbec1708a488928772f81e4792b1f6327f6a
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-08-29 18:37:37 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather (){
             <form>
                 <div className="row">
                     <div className="col-9">
-<input type ="search" placeholder="Enter a city..." className="form-control" />
+<input type ="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
 </div>
 <div className="col-3">
-<input type="submit" value = "Search" className="btn btn-primary"/>
+<input type="submit" value = "Search" className="btn btn-primary w-100"/>
 </div>
 </div>
             </form>
@@ -21,8 +21,12 @@ export default function Weather (){
             </ul>
 <div className= "row">
 <div className= "col-6">
-<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly cloudy"/>
-18°C
+<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly cloudy" />
+
+<span className="temperature">18</span>
+<span className="unit">°C</span>
+
+
 </div>
 <div className="col-6">
     <ul>
