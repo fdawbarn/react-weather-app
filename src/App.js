37f0160e13980f763392a9179ec15680fc9ae335
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-08-29 17:07:53 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App" >
+      <h1>
+        Weather app
+      </h1>
+      <footer>
 
+        This project was coded by Freya Dawbarn and is {""}
+        <a href="https://github.com/fdawbarn/react-weather-app" target="_blank">open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
