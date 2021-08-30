7652037eea4fda4508810d04b8328d642902fcf0
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-08-30 18:31:00 +0100

    Used the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ setweatherData({
     wind:response.data.wind.speed, 
     description:response.data.weather[0].description, 
     date: new Date(response.data.dt * 1000), 
-    iconUrl: 'https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png',
+    iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     city: response.data.name 
 }); 
 
