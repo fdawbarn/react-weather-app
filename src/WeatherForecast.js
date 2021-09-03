5e19f235a297d083bdf4e40a61dda381a38eccee
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-09-03 19:18:33 +0100

    Added useEffect to fix a bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
+
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+useEffect(()  => {
+setLoaded(false);
+}, [props.coordinates]); 
+
+
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -16,7 +24,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
